be8f412a535b14ea2a8e91745470540bc3cac8e9
kogajawa <kogajawa@gmil.com>
2025-01-31 12:21:39 +0300

    html css js

---
+++ main.js
@@ -0,0 +1,9 @@
+let img = document.getElementById('img0');
+let contener = document.querySelector('.contener')
+function phone(src){
+    img.src = src ;
+}
+function colors(color){
+    contener.style.background = color;
+
+}
